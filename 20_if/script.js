67f8dc81f99677f3f527c5422fe5cb641c219ed4
Gustavo Protachevicz <gustavo_sb.blz@hotmail.com>
2024-12-22 08:14:05 -0300

    js

---
+++ 20_if/script.js
@@ -0,0 +1,22 @@
+let idade = 19;
+
+if (idade == 19) {
+    console.log('A idade é = 19');
+}
+
+if(idade > 25) {
+    console.log('A idade é maior  que 25');
+}
+
+let nome = 'Gustavo';
+
+if (nome =='Gustavo' && idade > 10) {
+    console.log('Liberado!');
+}
+
+let passaporte = true;
+
+
+if ((nome =='Gustavo' && idade > 10) || passaporte == true) {
+    console.log('Liberado 2!');
+}
---
+++ 22_else_if/script.js
@@ -0,0 +1,11 @@
+let valor = 10;
+
+if (valor < 5) {
+    console.log('O valor é menor que 5');
+} else if (valor == 3)  {
+    console.log('O valor é igual a 3');
+} else if (valor != 10) {
+    console.log('O valor é diferente de 10');
+} else {
+    console.log(`O valor é ${valor}`);
+}
